3cc7bfb2f03368b03596111b552defa59ae19843
Taisaskov <95485685+Taisaskov@users.noreply.github.com>
2022-07-30 12:06:05 +0300

    Update 3.ВУЗ.py

--- 3.ВУЗ.py
+++ 3.ВУЗ.py
@@ -9,33 +9,18 @@ def isp(n):
 # функция, проверяющая кол-во делитей
 def delit(n):
     d = 2
-    # счетчик произведения делителей
-    k = 1
-    # счетчик кол-ва делителей
-    m = 0
+    k = 0
     while d*d < n:
         if n % d == 0:
             if isp(d):
-                k *= d
-                m += 1
-            if isp(n//d):
-                k *= 1
-                m += (n//d)
+                k += 1
+                if isp(n//d):
+                    k +=1
         d += 1
     if d*d == n and isp(d):
-        k *= d
-        m += 1
-    # проверка кол-ва делителей
-    if m == 5:
-        return k
-    else:
-        return 0
+        k += 1
+    return k
 # перебор значений
-for i in range(2,5000+1):
-    if delit(i) == i:
+for i in range(2,4620+1):
+    if delit(i) >= 5:
         print(i)
-
-        
-    
-        
-            
